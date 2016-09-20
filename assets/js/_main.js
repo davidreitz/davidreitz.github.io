4362eb15c4c5c9857d8f35f133fd56eeb22d6f91
rschaerer <github@p.uru.ch>
2016-09-20 13:49:04 +0200

    add return to top button (https://codepen.io/rdallaire/pen/apoyx)

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -97,4 +97,20 @@ $(document).ready(function(){
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
+  // return to top
+  // source: https://codepen.io/rdallaire/pen/apoyx
+  // ===== Scroll to Top ====
+  $(window).scroll(function() {
+      if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+          $('#return-to-top').fadeIn(200);    // Fade in the arrow
+      } else {
+          $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+      }
+  });
+  $('#return-to-top').click(function() {      // When arrow is clicked
+      $('body,html').animate({
+          scrollTop : 0                       // Scroll to top of body
+      }, 500);
+  });
+
 });
